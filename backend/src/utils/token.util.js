200d75e26f200c874e28ce45c8edc576aa787fde
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2025-08-15 02:06:55 +0530

    Update cookie sameSite and secure options for JWT
    Adjusts the 'sameSite' and 'secure' cookie options based on NODE_ENV. In non-development environments, 'sameSite' is set to 'none' and 'secure' is enabled to improve security and compatibility with cross-site requests.

--- backend/src/utils/token.util.js
+++ backend/src/utils/token.util.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV !== "development" ? "none" : "strict",
         secure: process.env.NODE_ENV !== "development"
     })
 
